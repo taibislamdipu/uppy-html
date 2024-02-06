3211e2179689ac8e250da9612b9d910fc563ac28
taibislamdipu <mailtaibislam@gmail.com>
2024-02-06 14:03:27 +0600

    on progress.

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
-var swiper = new Swiper(".mySwiper", {
+// var swiper = new Swiper(".mySwiper", {
+//   spaceBetween: 30,
+//   // centeredSlides: true,
+//   // autoplay: {
+//   //   delay: 2500,
+//   //   disableOnInteraction: false,
+//   // },
+//   loop: true,
+
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+var swiper = new Swiper("#swiperContainer", {
   spaceBetween: 30,
-  // centeredSlides: true,
-  // autoplay: {
-  //   delay: 2500,
-  //   disableOnInteraction: false,
-  // },
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   loop: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
-
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+  // Add other Swiper configurations as needed
 });
